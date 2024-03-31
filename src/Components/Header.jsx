e2dfbc646ff5216f11552fa64c4059aac9d99257
Aman056 <vijaychau056@gmail.com>
2024-03-31 18:09:24 +0530

    css fixing

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,21 +7,29 @@ import { useSelector } from 'react-redux';
 import img from '../Images/Main_icon.png'
 
 export default function Header() {
+    const navigate = useNavigate();
     const { Contact_ref, About_US_ref, Landing_ref } = useSelector((state) => state.Ref);
     const style = {
         color: "#00000"
     };
-
-    const handleNav = (ref_to) => {
+    const handleNav = async (ref_to) => {
+        document.getElementById('toggle-btn').click()
         if (window.location.pathname === '/') {
             if (ref_to instanceof HTMLElement) {
-                ref_to.focus();
-                ref_to.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+                ref_to.scrollIntoView({ behavior: 'smooth', block: 'start' });
             }
+        } else if (window.location.pathname === '/add-homestay') {
+            setTimeout(async () => {
+                await navigate('/');
+                if (ref_to instanceof HTMLElement) {
+                    await ref_to.click();
+                }
+            }, 1000); // 1 second delay before clicking ref_to
         }
     };
 
-    const navigate = useNavigate();
+
+
 
     return (
         <Navbar fixed="top" collapseOnSelect expand="lg" className="bg-body-tertiary p-0">
@@ -31,22 +39,22 @@ export default function Header() {
                         <img src={img} height={60} width={60} alt='home img' /> <span className='fs-3 px-2 home-stay-title'>HomeStay</span>
                     </div>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav px-3" />
+                <Navbar.Toggle id='toggle-btn' aria-controls="responsive-navbar-nav px-3" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto px-lg-4 mt-lg-2">
                         <Nav.Link>
-                            <Link onClick={() => window.location.pathname === '/' ? handleNav(Landing_ref) : navigate('/')} className="nav-link active add-new-property mx-lg-2" style={{ ...style, borderBottom: ".5px solid" }} aria-current="page" to="/"><span className='text px-2'>Home</span><span className='mt-2'><i class="bi bi-house"></i></span></Link>
+                            <Link onClick={() => handleNav(Landing_ref)} className="nav-link active add-new-property mx-lg-2" style={{ ...style, borderBottom: ".5px solid" }} aria-current="page" to="/"><span className='text px-2'>Home</span><span className='mt-2'><i class="bi bi-house"></i></span></Link>
                         </Nav.Link>
-                        <Nav.Link >
-                            <Link onClick={() => window.location.pathname === '/' ? handleNav(Contact_ref) : navigate('/')} className="nav-link active add-new-property  mx-lg-2" style={{ ...style, borderBottom: ".5px solid" }} ><span className='text px-3'>Contact us</span> <span className='mt-2 w-100'><i class="bi bi-telephone"></i></span></Link>
+                        <Nav.Link>
+                            <Link onClick={() => handleNav(Contact_ref)} className="nav-link active add-new-property  mx-lg-2" style={{ ...style, borderBottom: ".5px solid" }} ><span className='text px-3'>Contact us</span> <span className='mt-2 w-100'><i class="bi bi-telephone"></i></span></Link>
                         </Nav.Link>
-                        <Nav.Link >
-                            <Link onClick={() => window.location.pathname === '/' ? handleNav(About_US_ref) : navigate('/')} className="nav-link active add-new-property  mx-lg-2" style={{ ...style, borderBottom: ".5px solid" }} ><span className='text px-2'>About us</span> <span className='mt-2 w-100'><i class="bi bi-exclamation-circle"></i></span></Link>
+                        <Nav.Link>
+                            <Link onClick={() => handleNav(About_US_ref)} className="nav-link active add-new-property  mx-lg-2" style={{ ...style, borderBottom: ".5px solid" }} ><span className='text px-2'>About us</span> <span className='mt-2 w-100'><i class="bi bi-exclamation-circle"></i></span></Link>
                         </Nav.Link>
                     </Nav>
                     <Nav>
                         <Nav.Link>
-                            <Link to='/add-homestay' className="nav-link active add-new-property" style={{ ...style, borderBottom: "1.5px solid" }} >
+                            <Link onClick={()=>handleNav('data')} to='/add-homestay' className="nav-link active add-new-property" style={{ ...style, borderBottom: "1.5px solid" }} >
                                 <span className="text px-2">Add your property</span><span className='w-100 mt-2'>With HomeStay</span>
                             </Link>
                         </Nav.Link>
